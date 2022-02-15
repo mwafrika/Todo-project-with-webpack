e55c84fa047bd4a4498bf4617cb9620412d01a2c
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-02-16 00:17:39 +0200

    Add todo form and styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './style.css';
+import('./style.css');
+
 const todoList = [
   {
     id: 1,
@@ -29,16 +30,54 @@ const todoList = [
 
 const todo = () => {
   const todoListElement = document.querySelector('.todo-list');
-  const container = document.createElement('div');
+  const form = document.createElement('form');
+  const header = document.createElement('div');
+  const headerText = document.createElement('h1');
+  const clear = document.createElement('a');
+  clear.href = '#';
+  headerText.classList.add('title');
+  const headerImg = document.createElement('span');
+
+  headerText.innerHTML = "Today's To Do";
+  headerImg.innerHTML = 'Image';
+  header.classList.add('header');
+  const footer = document.createElement('div');
+  footer.classList.add('myFooter');
+  clear.innerHTML = 'Clear all completed';
+
+  const inputTodo = document.createElement('input');
+  inputTodo.placeholder = 'Add to your list...';
+  inputTodo.classList.add('input-todo');
+  footer.appendChild(clear);
+  header.appendChild(headerText);
+  header.appendChild(headerImg);
+  form.appendChild(header);
+  form.appendChild(inputTodo);
+
+  form.classList.add('form');
+  const mainContainer = document.createElement('div');
+  mainContainer.classList.add('main-container');
+
   todoList.forEach((todo) => {
     const todoElement = document.createElement('li');
     const { completed, description } = todo;
     todoElement.innerHTML = `
-            <input type="checkbox" ${completed ? 'checked' : ''} />
-            <span>${description}</span>
-            `;
+                  <div class='list-row'>
+                    <div class='content'>
+                        <input type="checkbox" ${
+                          completed ? 'checked' : ''
+                        } class='input' />
+                        <span>${description}</span>
+                    </div>
+                   <span class='icon'>Image</span>
+                    </div>
+                    `;
     todoListElement.appendChild(todoElement);
+    form.appendChild(todoListElement);
+    mainContainer.appendChild(form);
+    document.body.appendChild(mainContainer);
   });
+  form.insertBefore(footer, form.childNodes[3]);
 };
 
 window.addEventListener('DOMContentLoaded', todo);
