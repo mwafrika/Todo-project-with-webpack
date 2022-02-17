79a429c6a3cd99d719652f7df3417c4c435c74d0
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-02-17 04:09:04 +0200

    Todo list structure

--- src/index.js
+++ src/index.js
@@ -1,50 +1,28 @@
 import '@fortawesome/fontawesome-free/js/all.js';
-import {
-  todoList,
-  todoListElement,
-  form,
-  footer,
-  mainContainer,
-  outerHeader,
-  inputTodo,
-  header,
-  refreshImg,
-  headerText,
-  headerImg,
-  clear,
-  append,
-  inputContainer,
-} from './modules.js';
+import { todoList, todoListElement } from './modules.js';
 
 import('./style.css');
 
-// document.body.appendChild(outerHeader);
 const todo = () => {
-  append();
-
   todoList.forEach((todo) => {
     const { completed, description } = todo;
-    let todoElement = document.createElement('li');
-    todoElement.innerHTML = `
-                  <div class='list-row'>
+    const todoElement = document.createElement('li');
+    let list = '';
+    list += `
                     <div class='content'>
                        <input type="checkbox" ${
-                         completed ? 'checked' : ''
-                       } class='input' />
+  completed ? 'checked' : ''
+} class='input' />
                         <span>${description}</span>
                     </div>
                    <span class='icon'>
                    <i class="fa-solid fa-ellipsis-vertical"></i>
                    </span>
-                    </div>
                     `;
+    todoElement.innerHTML = list;
+    todoElement.classList.add('list-row');
     todoListElement.appendChild(todoElement);
-    form.appendChild(todoListElement);
-    mainContainer.appendChild(form);
-    document.body.appendChild(mainContainer);
   });
-
-  form.insertBefore(footer, form.childNodes[3]);
 };
 
 window.addEventListener('DOMContentLoaded', todo);
