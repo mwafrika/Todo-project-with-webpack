161cbcfb53b9c5513050b6ccda63cb3007e9d49f
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-02-18 20:04:07 +0200

    Add and remove functionalities

--- src/index.js
+++ src/index.js
@@ -1,28 +1,191 @@
 import '@fortawesome/fontawesome-free/js/all.js';
-import { todoList, todoListElement } from './modules.js';
 
-import('./style.css');
+import('./css/style.css');
 
-const todo = () => {
-  todoList.forEach((todo) => {
-    const { completed, description } = todo;
-    const todoElement = document.createElement('li');
-    let list = '';
-    list += `
-                    <div class='content'>
-                       <input type="checkbox" ${
-  completed ? 'checked' : ''
-} class='input' />
-                        <span>${description}</span>
-                    </div>
-                   <span class='icon'>
-                   <i class="fa-solid fa-ellipsis-vertical"></i>
-                   </span>
-                    `;
-    todoElement.innerHTML = list;
-    todoElement.classList.add('list-row');
-    todoListElement.appendChild(todoElement);
+/* DOMContentLoaded */
+document.addEventListener('DOMContentLoaded', main);
+
+/* main() FUNCTION */
+
+function main() {
+  // get alltodos and initialise listeners
+  addTodo();
+  // dragover on .todos container
+  document.querySelector('.todos').addEventListener('dragover', function (e) {
+    e.preventDefault();
+    if (
+      !e.target.classList.contains('dragging') &&
+      e.target.classList.contains('card')
+    ) {
+      const draggingCard = document.querySelector('.dragging');
+      const cards = [...this.querySelectorAll('.card')];
+      const currPos = cards.indexOf(draggingCard);
+      const newPos = cards.indexOf(e.target);
+      console.log(currPos, newPos);
+      if (currPos > newPos) {
+        this.insertBefore(draggingCard, e.target);
+      } else {
+        this.insertBefore(draggingCard, e.target.nextSibling);
+      }
+      const todos = JSON.parse(localStorage.getItem('todos'));
+      const removed = todos.splice(currPos, 1);
+      todos.splice(newPos, 0, removed[0]);
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
   });
-};
+  // add new todos on user input
+  const add = document.getElementById('add-btn');
+  const txtInput = document.querySelector('.txt-input');
+  add.addEventListener('click', function () {
+    const item = txtInput.value.trim();
+    if (item) {
+      txtInput.value = '';
+      const todos = !localStorage.getItem('todos')
+        ? []
+        : JSON.parse(localStorage.getItem('todos'));
+      const currentTodo = {
+        item,
+        isCompleted: false,
+      };
+      addTodo([currentTodo]);
+      todos.push(currentTodo);
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+    txtInput.focus();
+  });
+  // add todo also on enter key event
+  txtInput.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+      add.click();
+    }
+  });
 
-window.addEventListener('DOMContentLoaded', todo);
+  // clear completed
+  document
+    .getElementById('clear-completed')
+    .addEventListener('click', function () {
+      let deleteIndexes = [];
+      document.querySelectorAll('.card.checked').forEach(function (card) {
+        deleteIndexes.push(
+          [...document.querySelectorAll('.todos .card')].indexOf(card)
+        );
+        card.classList.add('fall');
+        card.addEventListener('animationend', function (e) {
+          setTimeout(function () {
+            card.remove();
+          }, 100);
+        });
+      });
+      removeManyTodo(deleteIndexes);
+    });
+}
+
+/* stateTodo() FUNCTION TO UPDATE TODO ABOUT COMPLETION */
+
+function stateTodo(index, completed) {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  todos[index].isCompleted = completed;
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+/* removeManyTodo() FUNCTION TO REMOVE ONE TODO */
+
+function removeTodo(index) {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  todos.splice(index, 1);
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+/* removeManyTodo FUNCTION TO REMOVE MANY TODOS */
+
+function removeManyTodo(indexes) {
+  let todos = JSON.parse(localStorage.getItem('todos'));
+  todos = todos.filter(function (todo, index) {
+    return !indexes.includes(index);
+  });
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+/* addTodo() FUNCTION TO LIST/CREATE TODOS AND ADD EVENT LISTENERS */
+
+function addTodo(todos = JSON.parse(localStorage.getItem('todos'))) {
+  if (!todos) {
+    return null;
+  }
+  const itemsLeft = document.getElementById('items-left');
+  // create cards
+  todos.forEach(function (todo) {
+    const card = document.createElement('li');
+    const cbContainer = document.createElement('div');
+    const cbInput = document.createElement('input');
+    const check = document.createElement('span');
+    const item = document.createElement('p');
+    const button = document.createElement('span');
+    const icon = document.createElement('i');
+
+    // Add classes
+    card.classList.add('card');
+    button.classList.add('clear');
+    cbContainer.classList.add('cb-container');
+    cbInput.classList.add('cb-input');
+    item.classList.add('item');
+    check.classList.add('check');
+    button.classList.add('clear');
+    icon.classList.add('fa', 'fa-times', 'clear');
+    // Set attributes
+    card.setAttribute('draggable', true);
+    cbInput.setAttribute('type', 'checkbox');
+    // set todo item for card
+    item.textContent = todo.item;
+    // if completed -> add respective class / attribute
+    if (todo.isCompleted) {
+      card.classList.add('checked');
+      cbInput.setAttribute('checked', 'checked');
+    }
+    // Add drag listener to card
+    card.addEventListener('dragstart', function () {
+      this.classList.add('dragging');
+    });
+    card.addEventListener('dragend', function () {
+      this.classList.remove('dragging');
+    });
+    // Add click listener to checkbox
+    cbInput.addEventListener('click', function () {
+      const correspondingCard = this.parentElement.parentElement;
+      const checked = this.checked;
+      stateTodo(
+        [...document.querySelectorAll('.todos .card')].indexOf(
+          correspondingCard
+        ),
+        checked
+      );
+      checked
+        ? correspondingCard.classList.add('checked')
+        : correspondingCard.classList.remove('checked');
+    });
+
+    // Add click listener to clear button
+    button.addEventListener('click', function () {
+      const correspondingCard = this.parentElement;
+      correspondingCard.classList.add('fall');
+      removeTodo(
+        [...document.querySelectorAll('.todos .card')].indexOf(
+          correspondingCard
+        )
+      );
+      correspondingCard.addEventListener('animationend', function () {
+        setTimeout(function () {
+          correspondingCard.remove();
+        }, 100);
+      });
+    });
+    // parent.appendChild(child)
+    button.appendChild(icon);
+    cbContainer.appendChild(cbInput);
+    cbContainer.appendChild(check);
+    card.appendChild(cbContainer);
+    card.appendChild(item);
+    card.appendChild(button);
+    document.querySelector('.todos').appendChild(card);
+  });
+}
--- src/modules.js
+++ src/modules.js
@@ -1,29 +1,3 @@
-export const todoList = [
-  {
-    id: 0,
-    description: 'Take my course at Udemy for 2 hours',
-    completed: false,
-  },
-  {
-    id: 1,
-    description: 'Complete the coding challenge',
-    completed: false,
-  },
-  {
-    id: 2,
-    description: 'Submit my pull request for the capstone',
-    completed: false,
-  },
-  {
-    id: 3,
-    description: 'Visit my freind at 5p.m today',
-    completed: false,
-  },
-  {
-    id: 4,
-    description: 'Attend the standup meeting at 6p.m',
-    completed: false,
-  },
-];
+export const todoList = [];
 
 export const todoListElement = document.querySelector('.todo-list');
